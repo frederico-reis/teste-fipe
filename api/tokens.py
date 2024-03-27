aaaaf702e81b1c16dab6e0d559121e344426b738
Frederico Abbud <fredericoabbud88@gmail.com>
2024-03-26 21:32:06 -0300

    🐛 corrige erro na expiração do token

---
+++ api/tokens.py
@@ -0,0 +1,26 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+from fastapi import HTTPException
+
+SECRET_KEY = "fipe"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  #Tempo de expiração do token em minutos
+
+def generate_token(user_id):
+
+    expiration_date = datetime.now(tz=timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data = {"user_id":user_id, "exp": expiration_date}
+    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm="HS256")
+
+    return encoded_jwt
+
+def verify_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expirado")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Token Inválido")
+    
+generate_token("oi")
